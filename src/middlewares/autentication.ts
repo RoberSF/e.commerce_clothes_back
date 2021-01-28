5c6a4a36a44d618566a9f2498a1ceddff0e3085c
rober_sanchez_figueroa <rober.sanchez.figueroa@gmail.com>
2021-01-28 23:42:43 +0100

    middleware to upload

---
+++ src/middlewares/autentication.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response  } from "express";
+import { SEED, SECRET_KEY } from '../config/constants';
+import jwt_decode from 'jwt-decode';
+import JWT from '../lib/jwt';
+var jwt = require('jsonwebtoken') // librería para crear el token 
+
+
+export const verificaToken = async ( req: Request, res: Response, next: NextFunction) => {
+
+    let token = req.headers.authorization || '';
+    if(!token ) {
+        return res.status(401).json({
+            status:false,
+            menssage: 'No hay token en la petición'
+    })
+    }
+
+    
+    try {
+        
+        const checkToken = new JWT().verify(token); // Ojo el secretKey
+        const user = Object.values(checkToken)[0]
+        if ( user.role === 'ADMIN') {
+            next();
+        }
+
+
+
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(401).json({
+            status:false,
+            menssage: 'No hay token en la petición'
+    })
+    }
+
+
+
+}
